b35a70600a2be12ccbd339d80e5d7c19d9a42c20
dartg <dartg@DESKTOP-32CQM3V>
2024-08-01 18:16:30 +0300

    Добавьте файлы проекта.

---
+++ Адовые объекты №26/Classas.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Адовые_объекты__25
+{
+    class Points
+    {
+        private int x;
+        private int y;
+        protected Polygon tochka;
+        private Brush color;
+
+        public Points(int x1, int y1)
+        {
+            x = x1;
+            y = y1;
+            tochka = new Polygon();
+            tochka.Stroke = Brushes.Aquamarine;
+            tochka.Fill = Brushes.Aquamarine;
+            tochka.StrokeThickness = 3;
+            tochka.Margin = new Thickness(x, y, 0, 0);
+            tochka.Points.Add(new Point(100, 100));
+            tochka.Points.Add(new Point(105, 100));
+            tochka.Points.Add(new Point(105, 105));
+            tochka.Points.Add(new Point(100, 105));
+
+        }
+        public void XYZ(ref Canvas canvas)
+        {
+            canvas.Children.Add(tochka);
+        }
+        public int x1
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        public int y1
+        {
+            get { return y; }
+            set { x = value; }
+        }
+        public void Show()
+        {
+            tochka.Visibility = Visibility.Visible;
+        }
+        public void Hide()
+        {
+            tochka.Visibility = Visibility.Hidden;
+        }
+        public void MoveXY(int newx, int newy)
+        {
+            x = newx; y = newy;
+            tochka.Margin = new Thickness(x, y, 0, 0);
+        }
+
+
+    }
+    class Quadro : Points
+    {
+        private int x;
+        private int y;
+        protected Polygon polygonq;
+        private Polygon polygon;
+        private Brush color;
+        public Quadro(int x1, int y1) : base(x1, y1)
+        {
+            x = x1;
+            y = y1;
+            polygonq = new Polygon();
+            polygonq.Stroke = Brushes.Aquamarine;
+            polygonq.StrokeThickness = 5;
+            polygonq.Points.Add(new Point(200, 200));
+            polygonq.Points.Add(new Point(400, 200));
+            polygonq.Points.Add(new Point(350, 100));
+            polygonq.Points.Add(new Point(150, 100));
+
+        }
+        public new void XYZ(ref Canvas canvas)
+        {
+            canvas.Children.Add(polygonq);
+        }
+        public int x1
+        {
+            get { return x; }
+            set { x = value; }
+
+        }
+        public int y1
+        {
+            get { return y; }
+            set { y = value; }
+        }
+        public new void Show()
+        {
+            polygonq.Visibility = Visibility.Visible;
+        }
+        public new void Hide()
+        {
+            polygonq.Visibility = Visibility.Hidden;
+        }
+        public new void MoveXY(int xCord, int yCord)
+        {
+            x1 = xCord;
+            y1 = yCord;
+            polygonq.Margin = new Thickness(x, y, 0, 0);
+        }
+
+
+    }
+    class QuadroFill : Points
+    {
+        private int x;
+        private int y;
+        protected Polygon polygonq;
+        private Polygon polygon;
+        private Brush color;
+        public QuadroFill(int x1, int y1) : base(x1, y1)
+        {
+            x = x1;
+            y = y1;
+            polygonq = new Polygon();
+            polygonq.Stroke = Brushes.Aquamarine;
+            polygonq.Fill = Brushes.Aqua;
+            polygonq.StrokeThickness = 5;
+            polygonq.Points.Add(new Point(200, 200));
+            polygonq.Points.Add(new Point(400, 200));
+            polygonq.Points.Add(new Point(350, 100));
+            polygonq.Points.Add(new Point(150, 100));
+
+        }
+        public new void XYZ(ref Canvas canvas)
+        {
+            canvas.Children.Add(polygonq);
+        }
+        public int x1
+        {
+            get { return x; }
+            set { x = value; }
+
+        }
+        public int y1
+        {
+            get { return y; }
+            set { y = value; }
+        }
+        public new void Show()
+        {
+            polygonq.Visibility = Visibility.Visible;
+        }
+        public new void Hide()
+        {
+            polygonq.Visibility = Visibility.Hidden;
+        }
+        public new void MoveXY(int xCord, int yCord)
+        {
+            x1 = xCord;
+            y1 = yCord;
+            polygonq.Margin = new Thickness(x, y, 0, 0);
+        }
+    }
+}
+
---
+++ Адовые объекты №26/MainWindow.xaml.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Адовые_объекты__25
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private Points Point;
+        private Quadro quadro;
+        private QuadroFill quadroFill;
+        private void Sozidat_Initialized(object sender, EventArgs e)
+        {
+            Point = new Points(25, 30);
+            Point.XYZ(ref cvas);
+            quadro = new Quadro(30, 30);
+            quadro.XYZ(ref cvas);
+            quadroFill = new QuadroFill(30, 30);
+            quadroFill.XYZ(ref cvas);
+            Point.Hide();
+            quadro.Hide();
+            quadroFill.Hide();
+        }
+
+        private void Letsgo_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            int x = Point.x1;
+            int y = Point.y1;
+
+            if (e.Key == Key.W)
+            {
+                if (y - 5 >= -201)
+                {
+                    Point.MoveXY(x, y - 3);
+                    quadro.MoveXY(x, y - 3);
+                    quadroFill.MoveXY(x, y - 3);
+                }
+            }
+            if (e.Key == Key.S)
+            {
+                if (y + 250 <= 460)
+                {
+                    Point.MoveXY(x, y + 3);
+                    quadro.MoveXY(x, y + 3);
+                    quadroFill.MoveXY(x, y + 3);
+                }
+            }
+            if (e.Key == Key.A)
+            {
+                if (x - 5 >= -83)
+                {
+                    Point.MoveXY(x - 3, y);
+                    Point.MoveXY(x - 3, y);
+                    Point.MoveXY(x - 3, y);
+                }
+            }
+            if (e.Key == Key.D)
+            {
+                if (x + 5 <= 225)
+                {
+                    Point.MoveXY(x + 3, y);
+                    Point.MoveXY(x + 3, y);
+                    Point.MoveXY(x + 3, y);
+                }
+            }
+            if (e.Key == Key.Up)
+            {
+                if (y - 5 >= -201)
+                {
+                    Point.MoveXY(x, y - 3);
+                    quadro.MoveXY(x, y - 3);
+                    quadroFill.MoveXY(x, y - 3);
+                }
+            }
+            if (e.Key == Key.Down)
+            {
+                if (y + 250 <= 460)
+                {
+                    Point.MoveXY(x, y + 3);
+                    quadro.MoveXY(x, y + 3);
+                    quadroFill.MoveXY(x, y + 3);
+                }
+            }
+            if (e.Key == Key.Left)
+            {
+                if (x - 5 >= -83)
+                {
+                    Point.MoveXY(x - 3, y);
+                    Point.MoveXY(x - 3, y);
+                    Point.MoveXY(x - 3, y);
+                }
+            }
+            if (e.Key == Key.Right)
+            {
+                if (x + 5 <= 225)
+                {
+                    Point.MoveXY(x + 3, y);
+                    Point.MoveXY(x + 3, y);
+                    Point.MoveXY(x + 3, y);
+                }
+            }
+
+        }
+
+        private void teleport_Click(object sender, RoutedEventArgs e)
+        {
+            Point.MoveXY(Convert.ToInt32(coord_X.Text), Convert.ToInt32(coord_Y.Text));
+            quadro.MoveXY(Convert.ToInt32(coord_X.Text), Convert.ToInt32(coord_Y.Text));
+            quadroFill.MoveXY(Convert.ToInt32(coord_X.Text), Convert.ToInt32(coord_Y.Text));
+        }
+
+        private void Point_Click(object sender, RoutedEventArgs e)
+        {
+            Point.Show();
+        }
+
+        private void Mongol_Click(object sender, RoutedEventArgs e)
+        {
+            quadro.Show();
+        }
+
+        private void ColorMongol_Click(object sender, RoutedEventArgs e)
+        {
+            quadroFill.Show();
+        }
+
+        private void Hide_Point_Click(object sender, RoutedEventArgs e)
+        {
+            Point.Hide();
+        }
+
+        private void Hide_Mongol_Click(object sender, RoutedEventArgs e)
+        {
+            quadro.Hide();
+        }
+
+        private void Hide_Color_Mongol_Click(object sender, RoutedEventArgs e)
+        {
+            quadroFill.Hide();
+        }
+
+        private void coord_X_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (coord_X.Text.Length == 0)
+                tp.IsEnabled = false;
+            else
+                tp.IsEnabled = true;
+        }
+
+        private void coord_Y_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            {
+                if (coord_Y.Text.Length == 0)
+                    tp.IsEnabled = false;
+                else
+                    tp.IsEnabled = true;
+            }
+
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            {
+                this.Close();
+            }
+        }
+
+        private void About_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Ваш друг черканул вам в тетради? Не беда! Cоздайте из точки многоугольник, а потом закрасьте его! Разработка от горе-дизайнера Бирюкова Георгия ИСП-23");
+        }
+
+        private void Anek_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Карлсон залетел и ждёт малыша");
+        }
+
+        private void skyrim_Click(object sender, RoutedEventArgs e)
+        {
+            Point.Show();
+            quadro.Show();
+        }
+
+        private void stels_Click(object sender, RoutedEventArgs e)
+        {
+            Point.Hide();
+            quadro.Hide();
+            quadroFill.Hide();
+        }
+    }
+
+
+
+    
+}
